2d81aca19859298f4bc088c757f8d61061577a8e
Babu Chandermani Rawat <BabuChandermaniRawat@users.noreply.github.com>
2021-05-10 20:03:55 +0530

    Used Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
-  let expenseDate = new Date(2021, 5, 28);
-  const expenseTitle = "Car Insaurance";
-  const expenseAmount = 294.5;
+function ExpenseItem(props) {
+  //let expenseDate = new Date(2021, 5, 28);
+  //const expenseTitle = "Car Insaurance";
+  //const expenseAmount = 294.5;
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
   );
