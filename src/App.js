f58f21c59b2f0246e9beb28eb8370d5400b60c76
Babu Chandermani Rawat <BabuChandermaniRawat@users.noreply.github.com>
2021-05-11 22:33:47 +0530

    Added Date Componenet and Expenses Componenet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-//import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
+//import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
+import "./components/Expenses.css";
 
 function App() {
   const expense = [
@@ -29,28 +31,8 @@ function App() {
     },
   ];
   return (
-    <div>
-      <h2>Let's get started</h2>
-      <ExpenseItem
-        title={expense[0].title}
-        amount={expense[0].amount}
-        date={expense[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[1].title}
-        amount={expense[1].amount}
-        date={expense[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[2].title}
-        amount={expense[2].amount}
-        date={expense[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expense[3].title}
-        amount={expense[3].amount}
-        date={expense[3].date}
-      ></ExpenseItem>
+    <div className="expenses">
+      <Expenses expense={expense}></Expenses>
     </div>
   );
 }
